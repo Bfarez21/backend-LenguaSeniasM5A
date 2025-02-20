c321cf45ebcaca393aab4fc07d5f6217df2da9d1
IsaacVilla11 <124637331+IsaacVilla11@users.noreply.github.com>
2025-02-19 22:29:05 -0500

    Creación del juego y nivel automatico
    EL juego se crea con id 1 y los niveles 1,2, 3 automaticamente al iniciar el back, el front solo funcionara con esos ids.

--- backendSenias/apps.py
+++ backendSenias/apps.py
@@ -4,3 +4,28 @@ from django.apps import AppConfig
 class BackendseniasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backendSenias'
+#Creacion de un juego con niveles por defecto al iniciar la API
+    def ready(self):
+        from .models import Juego, Nivel
+        if not Juego.objects.filter(nombre_juego='Adivina la seña').exists():
+            # Crear el juego
+            nuevo_juego, created = Juego.objects.get_or_create(
+                id=1,
+                nombre_juego='Adivina la seña',
+                defaults={'descripcion_jue': 'Adivina la seña mediante gifs con múltiples opciones'}
+            )
+
+            if created:
+                niveles = [
+                    {'id': 1, 'dificultad_nivel': 'Fácil', 'descripcion_nivel': 'Señas con números y letras'},
+                    {'id': 2, 'dificultad_nivel': 'Medio', 'descripcion_nivel': 'Señas de colores y familia'},
+                    {'id': 3, 'dificultad_nivel': 'Difícil', 'descripcion_nivel': 'Aprende imitando las señas'},
+                ]
+
+                for nivel in niveles:
+                    Nivel.objects.create(
+                        id=nivel['id'],
+                        dificultad_nivel=nivel['dificultad_nivel'],
+                        descripcion_nivel=nivel['descripcion_nivel'],
+                        FK_id_juego=nuevo_juego
+                    )
